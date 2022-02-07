c7cc53115cdb54b82571b74f56e7f5f4bc936b26
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-07 12:35:02 +0530

    Added: check for options method

--- src/snakeladder/SnakeAndLadder.java
+++ src/snakeladder/SnakeAndLadder.java
@@ -3,14 +3,33 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 	static int position = 0;
+	static final int LADDER = 1;
+	static final int SNAKE = 2;
+	static final int NOPLAY = 0;
 	public static int diceRoll() {
 		Random random = new Random();
 		int dice = random.nextInt(6)+1;
 		return dice;
 	}
+	public static void checkoption(int dice) {
+		Random random = new Random();
+		int check = random.nextInt(3);
+		if(check == LADDER) {
+			position = position+dice;
+		} else if(check == SNAKE) {
+			position = position-dice;
+		}
+		else {
+			position = position;
+		}
+	}
 
 	public static void main(String[] args) {
+		int dice = diceRoll();
 		System.out.println("Dice Roll number =" + diceRoll());
+		checkoption(dice);
+		System.out.println("Player current position = " + position);
+		
 	}
 
 }
