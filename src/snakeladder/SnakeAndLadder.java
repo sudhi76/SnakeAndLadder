dd5435694c7d693e4a473456b8f7e982b840ce3f
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-07 17:46:05 +0530

    Added:To exact winning 100

--- src/snakeladder/SnakeAndLadder.java
+++ src/snakeladder/SnakeAndLadder.java
@@ -1,4 +1,5 @@
 package snakeladder;
+
 import java.util.Random;
 
 public class SnakeAndLadder {
@@ -6,35 +7,35 @@ public class SnakeAndLadder {
 	static final int LADDER = 1;
 	static final int SNAKE = 2;
 	static final int NOPLAY = 0;
+
 	public static int diceRoll() {
 		Random random = new Random();
-		int dice = random.nextInt(6)+1;
+		int dice = random.nextInt(6) + 1;
 		return dice;
 	}
+
 	public static void checkoption(int dice) {
 		Random random = new Random();
 		int check = random.nextInt(3);
-		if(check == LADDER) {
-			position = position+dice;
-		} else if(check == SNAKE) {
-			position = position-dice;
-		}
-		else if (position < 0) {
+		if ((check == LADDER) && (position + dice <= 100)) {
+			position = position + dice;
+		} else if (check == SNAKE) {
+			position = position - dice;
+		} else if (position < 0) {
 			position = 0;
-		}
-		else {
+		} else {
 			position = position;
 		}
 	}
 
 	public static void main(String[] args) {
 		while (position < 100) {
-		int dice = diceRoll();
-		System.out.println("Dice Roll number =" + diceRoll());
-		checkoption(dice);
-		System.out.println("Player current position = " + position);
+			int dice = diceRoll();
+			System.out.println("Dice Roll number =" + diceRoll());
+			checkoption(dice);
+			System.out.println("Player current position = " + position);
 		}
-		
+
 	}
 
 }
