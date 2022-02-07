6d05472cf098daef37acf6243e63b15c1a0bd3b0
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-07 12:14:46 +0530

    Added: Dice role method

--- src/snakeladder/SnakeAndLadder.java
+++ src/snakeladder/SnakeAndLadder.java
@@ -1,10 +1,16 @@
 package snakeladder;
+import java.util.Random;
 
 public class SnakeAndLadder {
 	static int position = 0;
+	public static int diceRoll() {
+		Random random = new Random();
+		int dice = random.nextInt(6)+1;
+		return dice;
+	}
 
 	public static void main(String[] args) {
-
+		System.out.println("Dice Roll number =" + diceRoll());
 	}
 
 }
