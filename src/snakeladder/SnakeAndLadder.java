f0d6cd271f969b620aa19c4b152121e0117879c4
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-07 18:13:13 +0530

    Added: game with two player

--- src/snakeladder/SnakeAndLadder.java
+++ src/snakeladder/SnakeAndLadder.java
@@ -8,6 +8,10 @@ public class SnakeAndLadder {
 	static final int SNAKE = 2;
 	static final int NOPLAY = 0;
 	static int totalDiceCount = 0;
+	static final int PLAYER1 = 1;
+	static final int PLAYER2 = 2;
+	static int player = PLAYER1;
+	static int dice;
 
 	public static int diceRoll() {
 		Random random = new Random();
@@ -15,9 +19,14 @@ public class SnakeAndLadder {
 		return dice;
 	}
 
-	public static void checkoption(int dice) {
+	public static int checkoption() {
 		Random random = new Random();
 		int check = random.nextInt(3);
+		return check;
+	}
+
+	public static void positionCheck() {
+		int check = checkoption();
 		if ((check == LADDER) && (position + dice <= 100)) {
 			position = position + dice;
 		} else if (check == SNAKE) {
@@ -29,12 +38,18 @@ public class SnakeAndLadder {
 		}
 	}
 
+	public static void playercheck() {
+		int check = checkoption();
+
+	}
+
 	public static void main(String[] args) {
 		while (position < 100) {
 			totalDiceCount++;
-			int dice = diceRoll();
+			dice = diceRoll();
+			positionCheck();
 			System.out.println("Dice Roll number =" + diceRoll());
-			checkoption(dice);
+			checkoption();
 			System.out.println("Player current position = " + position);
 		}
 		System.out.println("total Dice Count:" + totalDiceCount);
